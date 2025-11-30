541532e806f78c39ef0544654c96d62ece97c765
HuynhThiNhuQuynh <buggbata039@gmail.com>
2025-11-30 16:36:30 +0700

    Add files via upload

---
+++ Ksan.java
@@ -0,0 +1,24 @@
+package com.mycompany.ksan;
+
+import com.mycompany.ksan.ui.CancelRoomUI;
+import com.mycompany.ksan.ui.CustomerUI;
+import com.mycompany.ksan.ui.HomeUI;
+import com.mycompany.ksan.ui.InformationHotelsUI;
+import com.mycompany.ksan.ui.RoomSelectionUI;
+import com.mycompany.ksan.ui.SeeReviewUI;
+
+
+
+/**
+ *
+ * @author PHUC HIEP
+ */
+public class Ksan {
+
+    public static void main(String[] args) {
+        HomeUI test = new HomeUI();
+        test.setVisible(true);
+        
+    }
+    
+}
